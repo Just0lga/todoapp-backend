07e4148b7d9eb78eb05fdf26d43b8e4abab2da3c
Just0lga <tkucukasci@gmail.com>
2024-10-14 21:52:03 +0300

    n

--- infrastructure/obj/Debug/net8.0/infrastructure.AssemblyInfo.cs
+++ infrastructure/obj/Debug/net8.0/infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bf3a425595e55bdeaf41a560ccaca7790af92dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
